eb70a6e08f2148f70be323634ff4da3e19e7a30e
mielesantos <mielesnts@gmail.com>
2024-11-20 19:26:51 -0300

    feat: format codigo

--- app/main.py
+++ app/main.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 """Curso LLMs Projeto 01 - Transcrição e compreensão de vídeos"""
 
-import os
 import io
+import os
 
+from dotenv import load_dotenv
 from langchain_community.document_loaders import YoutubeLoader
 from langchain_community.llms import HuggingFaceHub
-from langchain_openai import ChatOpenAI
-from langchain_ollama import ChatOllama
-
-from langchain_groq import ChatGroq
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 
 
 class AssistentTranscricao:
@@ -26,14 +25,14 @@ class AssistentTranscricao:
 
         # os.environ["HUGGINGFACEHUB_API_TOKEN"] = getpass.getpass()
 
-        if provedor_ai == "openai":
-            open_api_key = os.getenv("OPEN_API_KEY")
-            os.environ["OPENAI_API_KEY"] = open_api_key
+        if provedor_ai == 'openai':
+            open_api_key = os.getenv('OPEN_API_KEY')
+            os.environ['OPENAI_API_KEY'] = open_api_key
         else:
-            groq_api_key = os.getenv("GROQ_API_KEY")
-            os.environ["GROQ_API_KEY"] = groq_api_key
+            groq_api_key = os.getenv('GROQ_API_KEY')
+            os.environ['GROQ_API_KEY'] = groq_api_key
 
-    def format_transcricao(self, infos, transcricao: str) -> str:
+    def format_transcricao(self, infos, transcricao: str) -> str:  # noqa: PLR6301
         infos_video = f"""Informações do vídeo:
 
         Título: {infos[0].metadata['title']}
@@ -45,69 +44,76 @@ class AssistentTranscricao:
         """
         return infos_video
 
-    def salve_file_transcricao(self, infos, infos_formated: str):
-        with io.open("transcricao.txt", "w", encoding="utf-8") as f:
+    def salve_file_transcricao(self, infos, infos_formated: str):  # noqa: PLR6301
+        with io.open('transcricao.txt', 'w', encoding='utf-8') as f:
             for doc in infos:
                 f.write(infos_formated)
 
-    def model_hf_hub(
-        self, model="meta-llama/Meta-Llama-3-8B-Instruct", temperature=0.1
+    def model_hf_hub(  # noqa: PLR6301
+        self, model='meta-llama/Meta-Llama-3-8B-Instruct', temperature=0.1
     ):
         llm = HuggingFaceHub(
             repo_id=model,
             model_kwargs={
-                "temperature": temperature,
-                "return_full_text": False,
-                "max_new_tokens": 1024,
+                'temperature': temperature,
+                'return_full_text': False,
+                'max_new_tokens': 1024,
             },
         )
         return llm
 
-    def model_openai(self, model="gpt-4o-mini", temperature=0.1):
+    def model_openai(self, model='gpt-4o-mini', temperature=0.1):  # noqa: PLR6301
         llm = ChatOpenAI(model=model, temperature=temperature)
         return llm
 
-    def model_groq(
-        self, model="llama3-groq-70b-8192-tool-use-preview", temperature=0.1
+    def model_groq(  # noqa: PLR6301
+        self, model='llama3-groq-70b-8192-tool-use-preview', temperature=0.1
     ):
         llm = ChatGroq(model=model, temperature=temperature)
         return llm
 
-    def model_ollama(self, model="phi3", temperature=0.1):
+    def model_ollama(self, model='phi3', temperature=0.1):  # noqa: PLR6301
         llm = ChatOllama(model=model, temperature=temperature)
         return llm
 
     def llm_chain(self):
-        system_prompt = "Você é um assistente virtual prestativo e deve responder a uma consulta com base na transcrição de um vídeo, que será fornecida abaixo."
-
-        inputs = "Consulta: {consulta} \n Transcrição: {transcricao}"
-
-        if self.model_class.startswith("hf"):
-            user_prompt = "<|begin_of_text|><|start_header_id|>user<|end_header_id|>\n{}<|eot_id|><|start_header_id|>assistant<|end_header_id|>".format(
-                inputs
-            )
-        else:
-            user_prompt = "{}".format(inputs)
-
-        prompt_template = ChatPromptTemplate.from_messages(
-            [("system", system_prompt), ("user", user_prompt)]
+        system_prompt = (
+            'Você é um assistente virtual prestativo e deve responder a '
+            + 'uma consulta com base na transcrição de um vídeo, que será'
+            + ' fornecida abaixo.'
         )
 
-        ### Carregamento da LLM
-        if self.model_class == "hf_hub":
+        inputs = 'Consulta: {consulta} \n Transcrição: {transcricao}'
+
+        if self.model_class.startswith('hf'):
+            user_prompt = (
+                '<|begin_of_text|><|start_header_id|>user<|end_header_id|>'
+                + '\n{}<|eot_id|><|start_header_id|>assistant<|end_header_id|>'.format(
+                    inputs
+                )
+            )
+        else:
+            user_prompt = '{}'.format(inputs)
+
+        prompt_template = ChatPromptTemplate.from_messages([
+            ('system', system_prompt),
+            ('user', user_prompt),
+        ])
+
+        if self.model_class == 'hf_hub':
             llm = self.model_hf_hub()
-        elif self.model_class == "openai":
+        elif self.model_class == 'openai':
             llm = self.model_openai()
-        elif self.model_class == "ollama":
+        elif self.model_class == 'ollama':
             llm = self.model_ollama()
-        elif self.model_class == "groq":
+        elif self.model_class == 'groq':
             llm = self.model_groq()
 
         chain = prompt_template | llm | StrOutputParser()
 
         return chain
 
-    def get_video_info(self, url_video, language="pt", translation=None):
+    def get_video_info(self, url_video, language='pt', translation=None):  # noqa: PLR6301
         video_loader = YoutubeLoader.from_youtube_url(
             url_video,
             # add_video_info=True,
@@ -122,25 +128,25 @@ class AssistentTranscricao:
 
         return transcript, metadata
 
-    def interpret_video(self, url, query="resuma", language="pt", translation=None):
+    def interpret_video(self, url, query='resuma', language='pt', translation=None):
         try:
-            transcript, metadata = self.get_video_info(url, language, translation)
+            transcript, metadata = self.get_video_info(url, language, translation)  # noqa: F841
 
             chain = self.llm_chain()
 
-            res = chain.invoke({"transcricao": transcript, "consulta": query})
+            res = chain.invoke({'transcricao': transcript, 'consulta': query})
             print(res)
             # return res
         except Exception as e:
-            print("Erro ao carregar transcrição")
+            print('Erro ao carregar transcrição')
             print(e)
 
 
 # url_video = "https://www.youtube.com/watch?v=3LHSyha0xN0"
-url_video = input("Informe a url do video: ")
-query_user = "sumarize de forma clara de entender"
-model_class = "groq"
-language = ["pt", "pt-BR", "en"]
+url_video = input('Informe a url do video: ')
+query_user = 'sumarize de forma clara de entender'
+model_class = 'groq'
+language = ['pt', 'pt-BR', 'en']
 
 assitent = AssistentTranscricao(model_class=model_class)
 assitent.interpret_video(url_video, query_user, language)
